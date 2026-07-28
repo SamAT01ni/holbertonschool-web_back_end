bb46e78d2b15310c8ad62b63447e328ff63c0935
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-28 15:18:48 +1000

    5

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Bruce springsteen say im on fire
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """ sum """
+    return sum(input_list)
