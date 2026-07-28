c0f99f3204fc7f70f49da6c28690a4f768c9af06
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-28 15:42:15 +1000

    8

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+document this ya slag
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """ Making callable """
+    def multiply(n: float) -> float:
+        """ Make it a floaty """
+        return multiply
