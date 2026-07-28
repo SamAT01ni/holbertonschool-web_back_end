27b0a5e2d4e3c7651543391819cb8e56b521da38
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-28 15:22:24 +1000

    6

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+If a double decker bus
+crashes in to us
+"""
+
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    """ Lists and unions to make mixed """
+    return float(sum(mxd_lst))
