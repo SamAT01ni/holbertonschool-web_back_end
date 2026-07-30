180b384b4734ce90ad05085860f8dcfba4745a05
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-30 14:32:31 +1000

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 """
 comprehensions of async
+Making it appear longer for the craic
 """
 
 import asyncio
 import random
+from typing import Genertor
 
-
-async def async_generator():
-    """ Comments bleh """
+async def async_generator() -> Generator[float, None, None]:
+    """ Comments bleh bleh bleh i make longer """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
