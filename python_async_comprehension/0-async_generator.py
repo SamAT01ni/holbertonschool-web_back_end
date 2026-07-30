3ba41af26db8081986e4774c32afc9ddf0d19a57
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-07-30 14:13:55 +1000

    comments

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
+"""
+comprehensions of async
+"""
 
 import asyncio
 import random
 
 
 async def async_generator():
+    """ Comments bleh """
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
