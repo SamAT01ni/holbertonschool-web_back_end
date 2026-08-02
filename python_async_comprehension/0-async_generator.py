349490e127f8cbe5f351915a7760406d0abac989
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-02 14:08:02 +1000

    3 things

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -6,9 +6,9 @@ Making it appear longer for the craic
 
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """ Comments bleh bleh bleh i make longer """
     for i in range(10):
         await asyncio.sleep(1)
