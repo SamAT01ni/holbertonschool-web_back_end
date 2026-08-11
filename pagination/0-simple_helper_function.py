6a7a527b38bbf142de59c7d0076dfe46635c172b
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-11 15:00:17 +1000

    2 blank lines

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -3,6 +3,7 @@
 Index range returning 2 tuples containing a start index and end index
 """
 
+
 def index_range(page: int, page_size: int) -> tuple:
     """Takes integer arguments"""
     start = (page - 1) * page_size
