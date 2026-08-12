b8f89feb48776d93174e3806c704b2b11ad6bccc
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-12 11:28:36 +1000

    task 2 cieling

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -46,7 +46,7 @@ class Server:
                 str, Union[int, List, None]]:
         """This is a dictionary"""
         data = self.get_page(page, page_size)
-        total_pages = len(self.dataset()) / page_size if self.dataset() else 0
+        total_pages = math.ceil(len(self.dataset()) / page_size) if self.dataset() else 0
         next_page = page + 1 if page < total_pages else None
         prev_page = page - 1 if page > 1 else None
 
