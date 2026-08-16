2db3e2953fc3a6a9cff7b24aec4f00e29e54fd57
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-16 20:07:52 +1000

    silly error

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-"""Update topics"""
+"""Update topics with mingodb"""
 
 
 def update_topics(mongo_collection, name, topics):
     """I fixa da topics"""
     mongo_collection.update_many(
-        {'name': name}
-        {&set: {"topics": topics}}
+        {'name': name},
+        {'&set': {"topics": topics}}
     )
