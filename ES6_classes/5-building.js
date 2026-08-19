ae35a3c740e684238c0eedf313bfd737fb3037e4
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-19 14:25:51 +1000

    stack overflow the goat

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,8 @@
 export default class Building {
   constructor(sqft) {
     this._sqft = sqft;
-    if (this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+    if (this.constructor !== Building
+        && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
       throw new Error("Class extending Building must override evacuationWarningMessage");
     }
   }
@@ -11,7 +12,6 @@ export default class Building {
   }
 
   evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
-
+    return 'Get the f*** out!'
   }
 }
