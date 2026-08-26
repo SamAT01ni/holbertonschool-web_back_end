574bd033aa9d7abcf61182e73e7a671067d51367
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-26 12:41:30 +1000

    does this work

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,10 @@
 export default function cleanSet(set, startString) {
-  if (!startString) {
-    return '';
-  }
   const values = Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length));
 
+  if (!startString) {
+    return '';
+  }
   return values.join('-');
 }
