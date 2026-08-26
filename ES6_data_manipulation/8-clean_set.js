de05faa50d21512da517419221fc2c8f45e217e5
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-26 12:40:11 +1000

    got blank string

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
   const values = Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length));
