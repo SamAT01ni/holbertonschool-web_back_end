839b2d34a865520237929ef8d47b042b24560918
“SamAT01ni” <sam.a.thompson2001@gmail.com>
2026-08-27 15:07:46 +1000

    use brackets after the promise

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -2,10 +2,11 @@ import {uploadPhoto, createUser} from "./utils"
 
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto, createUser])
-    .then(([body, firstName, lastName]) => {
-      console.log(body, firstName, lastName);
-    }).catch(error => {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([photo, username]) => {
+      console.log(photo.body, username.firstName, username.lastName);
+    })
+    .catch(error => {
       console.error('Signup system offline');
     });
 }
